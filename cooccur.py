eee1ed48f698883224f24deddc35cd757f7b7bdf
Tristan Muntsinger <kachikosbodkin@gmail.com>
2016-04-21 22:00:16 -0700

    Add some more tests

--- cooccur.py
+++ cooccur.py
@@ -3,6 +3,8 @@
 import sys
 import numpy as np
 from scipy import spatial
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import cosine
 
 PREDICTION_MODEL = "b"
 
@@ -67,20 +69,63 @@ def predict(data, target):
 	
 	return np.round(prediction, 10)
 
-data = [[1, 0, 1, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0]]
-print("data:")
-print(np.array(data))
+def test1():
+	data = [[1, 0, 1, 0],
+	        [0, 1, 1, 0],
+	        [0, 1, 0, 0]]
+	print("data:")
+	print(np.array(data))
 
-for i in range(0,3):
-	target = [0, 0, 0, 0]
-	target[i] = 1
+	for i in range(0,3):
+		target = [0, 0, 0, 0]
+		target[i] = 1
 
-	prediction = predict(data, target)
+		prediction = predict(data, target)
 
-	print("target:")
-	print(np.array(target))
+		print("target:")
+		print(np.array(target))
 
-	print("prediction:")
-	print(prediction)
+		print("prediction:")
+		print(prediction)
+
+def test2():
+	data = np.array(
+		[[1, 1, 0, 1],
+		 [2, 0, 1, 1]])
+	
+	print("data:")
+	print(data)
+	
+	dist_out = 1 - pairwise_distances(data, metric="cosine")
+	print("cos_sim:")
+	print(dist_out)
+
+	items = np.array(data).astype(float)
+	matrix = np.dot(items.T, items)
+	matrix = np.dot(items, items.T)
+	print("items:")
+	print(items)
+	print("items.T:")
+	print(items.T)
+	print("matrix:")
+	print(matrix)
+	
+	# matrix = np.array(data).astype(float)
+	# cos_sim = matrix_cos_similarity(matrix)
+	
+	# print("cos_sim:")
+	# print(cos_sim)
+
+def test3():
+	data = [[1, 0, 1, 0],
+	        [0, 1, 1, 0],
+	        [0, 1, 0, 0]]
+	print("data:")
+	print(np.array(data))
+
+	cooccur = cooccurence(data, False, False, False)
+
+	print("cooccur:")
+	print(cooccur)
+
+test3()
